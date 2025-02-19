b80014d3271c7ede8bbc21adc48f0a6f9fc543b5
hoquocnam92 <hoquocnam92@gmail.com>
2025-02-19 12:04:19 +0700

    FIX OK

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const bars = document.querySelector('.bars')
 const overlay =document.querySelector('#overlay');
+const overlayImage =document.querySelector('.overlay');
 const sidebar = document.querySelector('#sidebar');
-console.log(bars);
+const gallery = document.querySelector('#check');
  
 function toggleSidebar () {
 [sidebar , overlay].forEach(function (e){
@@ -11,3 +12,24 @@ e.classList.toggle('active');
 [bars, overlay].forEach(function(e){
     e.addEventListener('click', toggleSidebar);
 })
+ 
+document.addEventListener('DOMContentLoaded' , ()=>{
+    const overlay = document.querySelector('#imageModal');
+    const close = document.querySelector('.close');
+    const modalImg = document.querySelector('#modalImg');
+    const gallery =document.querySelectorAll('.gallery-image');
+    gallery.forEach((e)=>{
+        e.addEventListener('click', (e)=>{
+            modalImg.src=e.target.src;
+            overlay.style.display='block';
+        });
+        overlay.addEventListener('click', (e)=>{
+            if(e.target===overlay){
+                overlay.style.display='none';
+            }
+        })
+        close.addEventListener('click', ()=>{
+            overlay.style.display='none';
+        });
+    })
+});
